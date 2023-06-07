54d565bfb5246f4c4e967c6c76422d737f3100e4
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-06-07 20:06:13 +0200

    mui packages and fix navbar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">WÖRKOUT</a>
+      <Link to="/" className="site-title">WÖRKOUT</Link>
       <ul>
         <li className="active">
-          <a href="/Quiz">Wörkout quiz</a>
+          <Link to="/Quiz">Wörkout quiz</Link>
         </li>
         <li>
-          <a href="/contact">Contact</a>
+          <Link to="/contact">Contact</Link>
         </li>
       </ul>
     </nav>
