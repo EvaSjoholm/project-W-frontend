4ba7b1510d2b8e01ae6faac20bf491377a5b811c
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-06-08 12:29:25 +0200

    icon and design

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -3,6 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from './Button';
 import { Loading } from './Quiz/Loading';
 
+import DbIcon from '../assets/DBicon.png'
+import foodIcon from '../assets/foodicon.png'
+import groupIcon from '../assets/groupicon.png'
+
 export const StartPage = ({ title }) => {
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate();
@@ -18,10 +22,25 @@ export const StartPage = ({ title }) => {
 
   return (
     <div className="main">
-
       <h1>{title}</h1>
-      <Button color="black" text="Take Quiz" onClick={onClickToQuiz} />
       {loading}
+      <div className="icon-container">
+        <ul className="icon-list-item">
+          <li className="icon-list-icon">
+            <img src={DbIcon} alt="Db-icon" height="40px" width="40px" />
+            <p> Here comes text</p>
+          </li>
+          <li className="icon-list-icon">
+            <img src={foodIcon} alt="food-icon" height="40px" width="40px" />
+            <p> Here comes text</p>
+          </li>
+          <li className="icon-list-icon">
+            <img src={groupIcon} alt="group-icon" height="40px" width="40px" />
+            <p> Here comes text</p>
+          </li>
+        </ul>
+        <Button color="black" text="Take Quiz" onClick={onClickToQuiz} />
+      </div>
     </div>
   )
 }
