1fdf5e3a305f48dfe8f3ddd905ca3dcf7c2a6e5b
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-06-09 10:42:25 +0200

    design fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import workoutImg from 'assets/workout.jpg'
+import blackImg from 'assets/blackandwhite.webp'
 
 export const Header = () => {
   return (
     <div className="header">
       <div className="header-container">
-        <img src={workoutImg} alt="header-img" height="200px" width="200px;" />
+        <div className="header-img">
+          <img src={blackImg} alt="header-img" />
+        </div>
         <div className="header-text">
           <h1> The Wörkout Effect</h1>
           <p>Move like water my friend</p>
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -21,26 +21,6 @@ export const StartPage = ({ title }) => {
       <h1>{title}</h1>
       <Button color="black" text="Take Quiz" onClick={onClickToQuiz} />
       {loading}
-      {/* <div className="icon-container">
-        <ul className="icon-list-item">
-          <li className="icon-list-icon">
-            <img src={DbIcon} alt="Db-icon" height="40px" width="40px" />
-            <span className="icon-text">
-              <p> Here comes text</p>
-            </span>
-          </li>
-
-          <li className="icon-list-icon">
-            <img src={foodIcon} alt="food-icon" height="40px" width="40px" />
-            <p> Here comes text</p>
-          </li>
-
-          <li className="icon-list-icon">
-            <img src={groupIcon} alt="group-icon" height="40px" width="40px" />
-            <p> Here comes text</p>
-          </li>
-        </ul>
-      </div> */}
     </div>
   )
 }
