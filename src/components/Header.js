497f061fe1d577e1eba7d0133edefd39b44efc33
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-06-09 11:23:10 +0200

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export const Header = () => {
     <div className="header">
       <div className="header-container">
         <div className="header-img">
-          <img src={blackImg} alt="header-img" />
+          <img src={blackImg} alt="header-img" height="80%" width="80%" />
         </div>
         <div className="header-text">
           <h1> The Wörkout Effect</h1>
