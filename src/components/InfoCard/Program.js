b56133826534e0aeb5df11d0e08295f56485d9bb
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-06-09 10:50:49 +0200

    added button onclick to contact page

--- src/components/InfoCard/Program.js
+++ src/components/InfoCard/Program.js
@@ -1,9 +1,16 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
 import Workout from 'assets/workout.jpg'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '../Button'
 import './Card.css'
 
 export const Program = () => {
+  const navigate = useNavigate();
+
+  const onClickToContact = () => {
+    navigate('/contact')
+  }
   return (
     <div className="program-section">
       <div className="class-card">
@@ -12,11 +19,12 @@ export const Program = () => {
         </div>
         <div className="class-text">
           <p>Join our WÖRKOUT family, fill out our QUIZ to get a ahead of your journey!</p>
-          <button
-            className="textbutton"
+          <Button
+            color="Darkgreen"
+            text="contact"
             type="button"
-            aria-label="button to take quiz for program search">See more
-          </button>
+            onClick={onClickToContact}
+            aria-label="button to take quiz for program search" />
         </div>
       </div>
     </div>
