b879c2e77720f28928181486aa95e314dca0e1ce
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-06-12 16:30:32 +0200

    summary design

--- src/components/Quiz/Summary.js
+++ src/components/Quiz/Summary.js
@@ -4,18 +4,19 @@ export const Summary = ({ answers }) => {
   console.log(answers)
 
   return (
-    <div className="summary-wrapper">
-      {answers.map((answer) => (
+    <div className="summary-container">
+      <div className="summary-wrapper">
+        {answers.map((answer) => (
         // eslint-disable-next-line no-underscore-dangle
-        <div key={answer.question}>
+          <div key={answer.question}>
 
-          <div className="summary-wrapper">
-            <h3 className="summary-h3"> {answer.question}</h3>
-            <p className="summary-p"> {answer.option}</p>
+            <div className="summary-wrapper">
+              <h3 className="summary-h3"> {answer.question}</h3>
+              <p className="summary-p"> {answer.option}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-
   );
 };
