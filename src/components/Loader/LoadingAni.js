d28dd200b0cb5d4f481cc73a079ee6123023d621
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-06-14 15:03:28 +0200

    loading ani design fix contact form

--- src/components/Loader/LoadingAni.js
+++ src/components/Loader/LoadingAni.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Player } from '@lottiefiles/react-lottie-player';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'components/Button';
+import './Loading.css'
 
 export const LoadingAni = () => {
+  const navigate = useNavigate()
+
   return (
-    <Player
-      autoplay
-      loop
-      src="https://assets4.lottiefiles.com/packages/lf20_wdetpydn.json"
-      style={{ height: '100px', width: '100px' }} />
+    <div className="ani-container">
+      <Player
+        autoplay
+        loop
+        src="https://assets4.lottiefiles.com/packages/lf20_wdetpydn.json"
+        style={{ height: '100px', width: '100px' }} />
+
+      <Button text="HOME" onClick={() => { navigate('/') }} />
+    </div>
   )
 }
