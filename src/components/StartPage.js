1073e12d0b86e461fdde7dcec3861463bece5b28
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-06-15 08:20:38 +0200

    mobile fix text startpage

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -15,19 +15,22 @@ export const StartPage = ({ title }) => {
       <div className="header">
         <div className="header-container">
           <div className="header-wrapper">
-            <h1> #WÖRKOUTFAMILY</h1>
-            <img src={WorkoutImg} alt="header-img" height="300px" width="300px" />
-          </div>
-          <div className="header-text">
-            <h1>LESS IS MORE</h1>
-            <p>Move like water my friend</p>
+            <div className="header-text">
+              <h1> #WÖRKOUTFAMILY</h1>
+
+              <img src={WorkoutImg} alt="header-img" height="300px" width="300px" />
+            </div>
+            <div className="header-text">
+              <h1>LESS IS MORE</h1>
+              <p>Move like water my friend</p>
+            </div>
           </div>
         </div>
       </div>
       <div className="box-div">
         <h1>{title}</h1>
-        <Button text="Take Quiz" onClick={onClickToQuiz} />
       </div>
+      <Button text="Take Quiz" onClick={onClickToQuiz} />
     </div>
   )
 }
